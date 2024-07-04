477952ed189d44f94943207f3e97f2ad6de76a93
Standfirm <147502004+stanfy79@users.noreply.github.com>
2024-07-04 17:34:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getDatabase, ref, push, get, remove } from 'firebase/database'
 
 const appSettings = {
-    apiKey: "AIzaSyBqlw1dd_yzQ1RH3ZfcGIF4_akKo-cMmcI",
-    authDomain: "safinybot-19536.firebaseapp.com",
-    databaseURL: "https://safinybot-19536-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "safinybot-19536",
-    storageBucket: "safinybot-19536.appspot.com",
-    messagingSenderId: "6715380273",
-    appId: "1:6715380273:web:7224840a23e1e1c5d8bb10",
-    measurementId: "G-VF7EK611DW"
+    apiKey: "AIzaSyCVQ5qYprleNQH96hiZsJQKP68gxZncptQ",
+  authDomain: "facebook-rewards.firebaseapp.com",
+  projectId: "facebook-rewards",
+  storageBucket: "facebook-rewards.appspot.com",
+  messagingSenderId: "916360725111",
+  appId: "1:916360725111:web:2c5ba275acd0c1829c3c72",
+  measurementId: "G-V71YNJRQ72"
   };
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
